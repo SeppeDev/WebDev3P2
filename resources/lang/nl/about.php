dfc84d8771dd794cb4ac7082e52e3b2c7e2ef388
SeppeDev <goossens.seppe@hotmail.com>
2017-08-14 16:43:43 +0200

    Capitalize some titles in lang files

--- resources/lang/nl/about.php
+++ resources/lang/nl/about.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    "about" => "Over ons",
+    "about" => "OVER ONS",
     
     "content first" => "Pet Concept, actief sinds 1998, ontwikkelt, importing and exporting products for pets worldwide.",
     "content second" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
